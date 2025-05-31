c55044044b77777aa3a625748fd99e6c66e3e2d9
mahdirnma <rahnamamahdi78@gmail.com>
2025-05-31 23:11:19 +0330

    fixed bugs

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,9 +3,55 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use App\Services\ApiResponseBuilder;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function __construct(public UserService $service){}
+
+    public function index()
+    {
+        $results = $this->service->getUsers();
+        return (new ApiResponseBuilder())->data(UserResource::collection($results->data))->response();
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
 }
